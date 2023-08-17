c082dab2fe40756fa16d4f6d13573c0cecb1de13
ibrahim-hisam <ibrahimhussam98@gmail.com>
2023-08-17 21:08:28 +0530

    First Changes

--- demo.js
+++ demo.js
@@ -1,1 +1,31 @@
-console.log('i love git');
+/* //console.dir(document);
+
+console.log(document.title)
+
+//document.title = 123
+
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+
+console.log(document.all[18]);
+console.log(document.forms); */
+
+//GETELEMENTBYID
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+/* console.log(document.getElementById('header-title'));
+var headerTitle = document.getElementById('header-title');
+console.log(headerTitle);
+headerTitle.textContent = 'Morning';
+headerTitle.innerText = 'Hello';
+headerTitle.innerHTML = 'Heyy'; */
+
+header.style.borderBottom = 'solid 3px #000';
+
+//GETELEMENTBYCLASSNAME
+var items = document.getElementsByClassName('title');
+console.log(items);
+console.log(items[0]);
+items[0].style.fontWeight = 'bold';
+items[0].style.color = 'green';
